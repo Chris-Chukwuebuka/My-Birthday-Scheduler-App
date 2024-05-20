e9a497865385b7a3c5ab710c5fdba7feece0de5b
Ebuka-Chucks <chrisbrandford4@gmail.com>
2024-05-20 15:30:58 +0100

    routes updated

--- client/index.js
+++ client/index.js
@@ -35,7 +35,7 @@ const scheduleBirthDay = async (event) => {
     }
 
     const response = await fetch(
-      "https://my-birthday-scheduler-app.onrender.com",
+      "https://my-birthday-scheduler-app.onrender.com/admin/birthday",
       {
         method: "POST",
         body: JSON.stringify(birthdayData),
